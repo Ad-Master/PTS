16eee294bdb63e6ed4650406e40636803c6cb9cc
Freesher <artur260@op.pl>
2015-03-11 18:08:07 +0100

    Test2

--- src/test/java/pl/grm/pts/core/misc/TestVectorUtils.java
+++ src/test/java/pl/grm/pts/core/misc/TestVectorUtils.java
@@ -11,7 +11,7 @@ public class TestVectorUtils {
 	
 	@Test
 	public final void testToComplexVector() {
-		String txt = "[2 3 0 2+2j 3-2j j]";
+		String txt = "[2 3 0 2+2j 3-2j j 3-3j 2-4j]";
 		Vector<Complex> complexVector = VectorUtils.toComplexVector(txt);
 		assertThat(complexVector.get(0).getReal(), is(2D));
 		assertThat(complexVector.get(0).getImaginary(), is(0D));
@@ -20,6 +20,24 @@ public class TestVectorUtils {
 		assertThat(complexVector.get(4).getReal(), is(3D));
 		assertThat(complexVector.get(4).getImaginary(), is(-2D));
 		assertThat(complexVector.get(5).getReal(), is(0D));
-		// assertThat(complexVector.get(5).getImaginary(), is(1D)); //FIXME
+		assertThat(complexVector.get(5).getImaginary(), is(0D)); 
+		assertThat(complexVector.get(6).getReal(), is(3D));
+		assertThat(complexVector.get(6).getImaginary(), is(-3D)); 
+		assertThat(complexVector.get(7).getReal(), is(2D));
+		assertThat(complexVector.get(7).getImaginary(), is(-4D)); 
+		//Tutaj wszystko dziala.
+		
+		
 	}
+	public final void Round(){
+		//Tutaj nie wiem jak zabrac sie za to!
+		Round round = VectorUtils.round();
+	}
+	
+	
+	
+	
+		
+	
+	
 }
